0d543c845a206ad4e3a24bd317319c595c6ed92b
Younes-Elouarouar <elouarouaryounes1@gmail.com>
2024-09-13 11:30:30 +0100

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,5 @@
 principal = float(1000)  #representing $1000
 rate = float(0.05)  #representing 5% annual interest
 time = float(3)   #representing 3 years
-interest print( principal * rate * time )
+interest = print( principal * rate * time )
 
