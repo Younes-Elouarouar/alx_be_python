c16ec2f28b11d56a80258e90b74acd7c5a460780
Younes-Elouarouar <elouarouaryounes1@gmail.com>
2024-09-13 11:18:40 +0100

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,4 +1,10 @@
 number1 = 10
-number2 = 5
-result = number1 * number2
-print (result)
+number2 = 2
+result1 = number1 - number2
+result2 = number1 + number2
+
+result3 = number1 * number2
+print (result1)
+print (result2)
+print (result3)
+
