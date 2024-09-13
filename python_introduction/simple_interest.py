f78ce9672fd3903860230769d2c3245f991db294
Younes-Elouarouar <elouarouaryounes1@gmail.com>
2024-09-13 11:33:45 +0100

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,6 @@
-principal = int(1000)  #representing $1000
-rate = float(0.05)  #representing 5% annual interest
-time = int(3)   #representing 3 years
-interest = int(print( principal * rate * time )(
-
+# Define the principal, rate, and time variables
+principal = 1000
+rate = 0.05
+time = 3
+interest = principal * rate * time
+print(f"The simple interest is: {interest}")
