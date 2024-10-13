42875e00550da3a820997dba719a41ee3dbec994
Younes-Elouarouar <elouarouaryounes1@gmail.com>
2024-10-13 14:55:03 +0100

    Create main.py

---
+++ oop/main.py
@@ -0,0 +1,9 @@
+from book_class import Book
+
+def main():
+    my_book = Book("1984", "George Orwell", 1949)
+    print(my_book)  # Expected to use __str__
+    print(repr(my_book))  # Expected to use __repr__
+    del my_book
+if __name__ == "__main__":
+    main()
